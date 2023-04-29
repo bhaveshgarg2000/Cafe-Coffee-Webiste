81a40216747c0f26c9b74006bbb54241c7ff1871
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2023-04-29 23:26:01 +0530

    Chnage

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() {
         return (
-            <img src="https://cdnimg.webstaurantstore.com/uploads/blog/2019/4/coffee-drinks-infographic.png" />
+            <img style={{width: '100%'}} src="https://cdnimg.webstaurantstore.com/uploads/blog/2019/4/coffee-drinks-infographic.png" />
         );
     }
 }
